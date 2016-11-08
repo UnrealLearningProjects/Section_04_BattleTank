8e002c164e55d0e8077709c66642e42cc96d2dda
Paaatreeeck <patrick.drinkwalter@freenet.de>
2016-11-08 17:00:40 +0100

    Setting up Online/Remote Repository; Setting up Project for Section 04

---
+++ MyBattleTank/Source/MyBattleTank/MyBattleTankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MyBattleTankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYBATTLETANK_API AMyBattleTankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
